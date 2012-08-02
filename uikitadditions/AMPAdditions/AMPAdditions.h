1b70a243a37b7e677a98a3fbfb442a8e177c9e88
Alejandro Martinez <alexito4@gmail.com>
2012-08-02 17:32:22 +0200

    Rename

---
+++ uikitadditions/AMPAdditions/AMPAdditions.h
@@ -0,0 +1,14 @@
+//
+//  AMPAdditions.h
+//  uikitadditions
+//
+//  Created by Alejandro Martinez on 02/08/12.
+//  Copyright (c) 2012 Alejandro Martinez. All rights reserved.
+//
+
+#ifndef uikitadditions_AMPAdditions_h
+#define uikitadditions_AMPAdditions_h
+
+
+
+#endif
