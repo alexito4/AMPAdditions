f66b4c6197162888fefbb2f6e97ea50d12013629
Alejandro Martinez <alexito4@gmail.com>
2012-08-17 15:40:31 +0200

    Clean folders

--- uikitadditions/AppDelegate.h
+++ uikitadditions/AppDelegate.h
@@ -12,6 +12,5 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) UITabBarController *tabBarController;
 
 @end
--- uikitadditions/AppDelegate.m
+++ uikitadditions/AppDelegate.m
@@ -10,8 +10,6 @@
 
 #import "FirstViewController.h"
 
-#import "SecondViewController.h"
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -19,10 +17,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     UIViewController *viewController1 = [[FirstViewController alloc] initWithNibName:@"FirstViewController" bundle:nil];
-    UIViewController *viewController2 = [[SecondViewController alloc] initWithNibName:@"SecondViewController" bundle:nil];
-    self.tabBarController = [[UITabBarController alloc] init];
-    self.tabBarController.viewControllers = @[viewController1, viewController2];
-    self.window.rootViewController = self.tabBarController;
+    
+    self.window.rootViewController = viewController1;
     [self.window makeKeyAndVisible];
     return YES;
 }
