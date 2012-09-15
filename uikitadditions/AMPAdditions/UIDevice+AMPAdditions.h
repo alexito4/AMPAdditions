947d851851a50877dca0a3aa09279b846f4459c9
Alejandro Martinez <alexito4@gmail.com>
2012-09-15 17:11:36 +0200

    UIDevice -isPhone and -isPad

---
+++ uikitadditions/AMPAdditions/UIDevice+AMPAdditions.h
@@ -0,0 +1,16 @@
+//
+//  UIDevice+AMPAdditions.h
+//  walipa
+//
+//  Created by Alejandro Martinez on 15/09/12.
+//  Copyright (c) 2012 Alejandro Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (AMPAdditions)
+
+- (BOOL)isPhone;
+- (BOOL)isPad;
+
+@end
---
+++ uikitadditions/AMPAdditions/UIDevice+AMPAdditions.m
@@ -0,0 +1,21 @@
+//
+//  UIDevice+AMPAdditions.m
+//  walipa
+//
+//  Created by Alejandro Martinez on 15/09/12.
+//  Copyright (c) 2012 Alejandro Martinez. All rights reserved.
+//
+
+#import "UIDevice+AMPAdditions.h"
+
+@implementation UIDevice (AMPAdditions)
+
+- (BOOL)isPhone {
+    return ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone);
+}
+
+- (BOOL)isPad {
+    return ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad);
+}
+
+@end
