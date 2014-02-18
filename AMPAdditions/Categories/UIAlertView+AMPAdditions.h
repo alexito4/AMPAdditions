c763497affe5aaf73cdb19c6d64c7d71a24ccc2a
Alejandro Martinez <alexito4@gmail.com>
2014-02-18 12:20:50 +0100

    Added block support to UIAlertView
    I added (again) block support to UIAlertView. But now, with no
    dependencies! Just the objc runtime.
    Also, now I’m using and NSArray for the ‘other buttons’ so you can use
    the new objc syntax -> @[@“”button 1] and forget about sentinels.

--- AMPAdditions/Categories/UIAlertView+AMPAdditions.h
+++ AMPAdditions/Categories/UIAlertView+AMPAdditions.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void (^AMPAlertViewCompletionBlock) (UIAlertView *alertView, NSInteger buttonIndex);
+
 @interface UIAlertView (AMPAdditions)
 
 + (void)showAlertViewWithTitle:(NSString *)title message:(NSString *)message cancelButtonTitle:(NSString *)cancel;
+
++ (void)showAlertViewWithTitle:(NSString *)title
+                       message:(NSString *)message
+             cancelButtonTitle:(NSString *)cancelTitle
+             otherButtonTitles:(NSArray *)otherTitles
+               completionBlock:(AMPAlertViewCompletionBlock)completionBlock;
+
+@property (copy, nonatomic) AMPAlertViewCompletionBlock completionBlock;
 
 @end
--- AMPAdditions/Categories/UIAlertView+AMPAdditions.m
+++ AMPAdditions/Categories/UIAlertView+AMPAdditions.m
@@ -6,12 +6,62 @@
 //  Copyright (c) 2014 Alejandro Martinez. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import "UIAlertView+AMPAdditions.h"
+
+static const void *kAMPAlertViewCompletionBlock = @"kAMPAlertViewCompletionBlock";
 
 @implementation UIAlertView (AMPAdditions)
 
 + (void)showAlertViewWithTitle:(NSString *)title message:(NSString *)message cancelButtonTitle:(NSString *)cancel {
     [[[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:cancel otherButtonTitles:nil] show];
+}
+
++ (void)showAlertViewWithTitle:(NSString *)title
+                       message:(NSString *)message
+             cancelButtonTitle:(NSString *)cancelTitle
+             otherButtonTitles:(NSArray *)otherTitles
+               completionBlock:(AMPAlertViewCompletionBlock)completionBlock {
+
+    UIAlertView *alertView = [[self alloc] initWithTitle:title
+                                                 message:message
+                                                delegate:nil
+                                       cancelButtonTitle:cancelTitle
+                                       otherButtonTitles:[otherTitles firstObject], nil];
+
+    if (otherTitles.count > 1) {
+        for (NSString *buttonTitle in [otherTitles subarrayWithRange:NSMakeRange(1, otherTitles.count - 1)]) {
+            [alertView addButtonWithTitle:buttonTitle];
+        }
+    }
+
+    if (completionBlock) {
+        [alertView setCompletionBlock:completionBlock];
+    }
+
+    [alertView show];
+}
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    AMPAlertViewCompletionBlock completion = alertView.completionBlock;
+
+    if (completion) {
+        completion(alertView, buttonIndex);
+    }
+}
+
+#pragma mark - Properties
+
+- (AMPAlertViewCompletionBlock)completionBlock {
+    return objc_getAssociatedObject(self, kAMPAlertViewCompletionBlock);
+}
+
+- (void)setCompletionBlock:(AMPAlertViewCompletionBlock)completionBlock {
+    self.delegate = self;
+    
+    objc_setAssociatedObject(self, kAMPAlertViewCompletionBlock, completionBlock, OBJC_ASSOCIATION_COPY);
 }
 
 @end
